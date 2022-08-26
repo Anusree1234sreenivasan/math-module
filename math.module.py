9e3ab2e457078c6d0b600d16ab268125496bae8d
Anusree1234sreenivasan <110896832+Anusree1234sreenivasan@users.noreply.github.com>
2022-08-26 15:24:41 +0530

    FIRST COMMIT

---
+++ math.module.py
@@ -0,0 +1,76 @@
+import math
+print(math.pi)
+print(math.inf)
+print("________________________________________")
+print(math.ceil(1.4)) #mukalill ulla integer number
+print(math.floor(1.4)) #thazha ulla intergers number
+print(math.ceil(5.3))
+print(math.floor(5.3))
+print(math.ceil(-5.3))
+print(math.floor(-5.3))
+print(math.ceil(22.6))
+print(math.floor(22.6))
+print(math.ceil(10.0))
+print(math.floor(10.0))
+print(math.ceil(10.2))
+print(math.floor(10.4))
+print(math.ceil(3.231))
+print("___________________________________________")
+
+print(math.cos(0))
+print(math.cos(10))
+print(math.cos(3.14159265359))
+print(math.sin(0))
+print(math.sin(10))
+print(math.sin(3.14689))
+
+print("_______________________________________")
+
+print(math.degrees(8.90)) #radian to degree
+print(math.degrees(-20))
+print(math.degrees(1))
+print(math.degrees(90))
+
+print("___________________________")
+
+print(math.fabs(-66.43)) #absolute value
+print(math.fabs(-7))
+
+print("-----------------------------------")
+
+print(math.fmod(20,4))#mode calcaluation
+print(math.fmod(9,6))
+print(math.fmod(12,7))
+
+print("----------------------------------")
+
+print(math.fsum([1,2,3,4,5])) #sum 1+2+3+4+5
+
+print("-----------------------------")
+
+print(math.prod([1,2,3,4,5]))  #product 1*2*3*4*5
+
+print("---------------------------")
+
+print(math.gcd(5,10,15,20)) #greatest common divicer#common ayitt divide cheyyan pattunna value,
+
+print("-----------------------------")
+
+print(math.isfinite(5))
+
+print("_------------------------------------")
+
+print(math.isinf(10))
+
+print(math.isinf(math.inf))
+
+print("_------------------------------------------------")
+
+print(math.isnan(float("nan")))
+print(math.isnan(5))
+print(math.isnan(math.nan))
+
+print("-------------------------------------")
+print(math.pow(3,9))
+print("---------------------------------")
+print(math.sqrt(25))
